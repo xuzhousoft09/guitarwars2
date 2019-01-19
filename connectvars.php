37bb31081461b8c240210edd34acf81d9b5a0570
qiang.lv <xzsdeveloper@gmail.com>
2019-01-19 16:33:34 +0800

    add new functions

---
+++ connectvars.php
@@ -0,0 +1,7 @@
+<?php
+  // Define database connection constants
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASSWORD', '123456');
+  define('DB_NAME', 'gwdb');
+?>
